c4795b8826b3dad43ce516962c39273e3e2af101
Adrian Dawid <adriandwd@gmail.com>
2016-08-28 00:54:28 +0200

    Bugfixes

--- diff_eq.py
+++ diff_eq.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
+
 class DifferentialQuantity():
     def __init__(self,id,eq):
         self.id = id
@@ -12,7 +13,7 @@ class DifferentialQuantity():
         if not self.is_defined:
             raise Exception("Quantity is not yet defined.")
 
-        if grade < len(self.derivatives):
+        if grade > len(self.derivatives):
             raise Exception("Requested derivative not defined.")
 
         return self.derivatives[grade]
@@ -24,11 +25,12 @@ class DifferentialQuantity():
         self.derivatives = derivatives
         self.is_defined = True
 
+
 class DifferentialEquation():
     def __init__(self):
         self.quantities = []
 
-    def generate_quantities(self,N):
+    def generate_quantities(self, N):
         new_quantities = []
         for i in range(N):
             x = DifferentialQuantity(len(self.quantities)+1,self)
@@ -36,23 +38,29 @@ class DifferentialEquation():
             self.quantities.append(x)
         return new_quantities
 
-    def define_quantity(self,quantity,initial_value,initial_value_order,definition,definition_order):
+    def define_quantity(self, quantity, initial_value, initial_value_order, definition_order):
         if not quantity.eq == self:
             raise Exception("The supplied quantity is not part of this differential equation.")
 
-        if initial_value_order <= definition_order:
-            raise Exception("The initial value has to be assigned to a higher derivative than is being defined.")
+        if initial_value_order >= definition_order:
+            raise Exception("The initial value has to be assigned to a lower-order derivative than is being defined.")
         derivatives = []
-        for n in range(definition_order - 1):
+        for n in range(definition_order):
             dx = None
             if n == initial_value_order:
-                dx = tf.Variable(initial_value)
+                dx = tf.Variable(np.reshape(initial_value, np.size(initial_value)), dtype="float64")
             else:
                 dx = tf.Variable(np.zeros(np.size(initial_value)))
             derivatives.append(dx)
-        derivatives.append(definition)
+        quantity.define(derivatives)
 
-    def generate_simulate_operation(self,dt,step_size):
+        def define_op(definition):
+            derivatives.append(definition)
+            quantity.define(derivatives)
+
+        return define_op
+
+    def generate_simulate_operation(self, dt):
         # Are all quantities defined?
         for quantity in self.quantities:
             if not quantity.is_defined:
@@ -62,7 +70,8 @@ class DifferentialEquation():
         for quantity in self.quantities:
             for n in range(len(quantity.derivatives)-1):
                 updates.append(quantity.derivatives[n].assign(quantity.derivatives[n] + dt * (quantity.derivatives[n + 1])))
-        return tf.group(updates)
+        updates_combined = tf.tuple(updates)
+        return updates_combined
 
                 
 
